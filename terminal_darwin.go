45c81214c3ea40152771a900077e5ea354797c68
OpsRamp <40623644+opsramp@users.noreply.github.com>
2018-06-28 19:29:41 +0530

    Update terminal_darwin.go
    added GetCurrentThreadId() function definition

--- terminal_darwin.go
+++ terminal_darwin.go
@@ -3,8 +3,24 @@
 
 package logrus
 
-import "golang.org/x/sys/unix"
+import (
+  "golang.org/x/sys/unix"
+  "string"
+  "runtime"
+  "strconv"  
+)
 
 const ioctlReadTermios = unix.TIOCGETA
 
 type Termios unix.Termios
+
+func GetCurrentThreadId() int {
+	var buf [64]byte
+	n := runtime.Stack(buf[:], false)
+	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
+	id, err := strconv.Atoi(idField)
+	if err != nil {
+		return 0
+	}
+	return id
+}
