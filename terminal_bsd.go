f6ecce23bfb7c9d4e54e21ed6d538537785f782e
OpsRamp <40623644+opsramp@users.noreply.github.com>
2018-06-28 19:31:20 +0530

    Update terminal_bsd.go

--- terminal_bsd.go
+++ terminal_bsd.go
@@ -5,6 +5,12 @@ package logrus
 
 import "golang.org/x/sys/unix"
 
+Import “syscall”
+
 const ioctlReadTermios = unix.TIOCGETA
 
 type Termios unix.Termios
+
+func GetCurrentThreadId() int {
+	return syscall.Gettid()
+}
