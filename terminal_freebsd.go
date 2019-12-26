ff440d17f0b83e65ec65c99ee10c0efc7bb3cc8f
shivshankarsingh-opsramp <shivshankar.singh@opsramp.com>
2019-12-26 13:29:18 +0530

    returned unix.Getpid() instead of thread id

---
+++ terminal_freebsd.go
@@ -0,0 +1,14 @@
+// +build freebsd openbsd netbsd dragonfly
+// +build !appengine,!gopherjs
+
+package logrus
+
+import "golang.org/x/sys/unix"
+
+const ioctlReadTermios = unix.TIOCGETA
+
+type Termios unix.Termios
+
+func GetCurrentThreadId() int {
+	return unix.Getpid()
+}
